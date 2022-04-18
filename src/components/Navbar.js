3bf13ba9780c278f9cf48ce65742df8aca690e47
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-18 10:33:40 -0400

    Pre-router draft

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,20 @@
+import TopStories from './TopStories'
+import TopBooks from './TopBooks'
+
+const NavBar = () => {
+
+    return (
+        <div>
+            <h3>Homepage</h3>
+            <h3>Top Stories</h3>
+            <TopStories />
+            <h3>Top Books</h3>
+            <TopBooks />
+        </div>
+    )
+}
+
 export default Navbar;
+
+//import BrowserRouter from react-router-dom
+//switch statement, defaulting to TopStories
