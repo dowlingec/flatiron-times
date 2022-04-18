66d85e15c2cfbc848613729914134d6bdebc6183
Justin Chazen <Justinchazen96@gmail.com>
2022-04-18 13:14:19 -0400

    moved buttons to TopBooks

--- src/components/TopBooks.js
+++ src/components/TopBooks.js
@@ -1,7 +1,49 @@
-const TopBooks = () => {
+const TopBooks = ({setBookType}) => {
     return(
         <div>
+                        <button onClick={()=>{
+                setBookType('combined-print-and-e-book-fiction')
+                }}>Fiction</button>
 
+            <button onClick={()=>{
+                setBookType('combined-print-and-e-book-nonfiction')
+            }}>Non-Fiction</button>
+
+            <button onClick={()=>{
+                setBookType('manga')
+            }}>Manga</button>
+            
+            <button onClick={()=>{
+                setBookType('animals')
+            }}>Animals</button>
+
+            <button onClick={()=>{
+                setBookType('business-books')
+            }}>Business</button>
+
+            <button onClick={()=>{
+                setBookType('education')
+            }}>Education</button>
+
+            <button onClick={()=>{
+                setBookType('food-and-fitness')
+            }}>Food & Fitness</button>
+
+            <button onClick={()=>{
+                setBookType('humor')
+            }}>Humor</button>
+
+            <button onClick={()=>{
+                setBookType('science')
+            }}>Science</button>
+
+            <button onClick={()=>{
+                setBookType('sports')
+            }}>Sports</button>
+
+            <button onClick={()=>{
+                setBookType('travel')
+            }}>Travel</button>
         </div>
     )
 }
