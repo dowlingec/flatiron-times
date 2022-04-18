24ce2413beb9d6514deb8b5c5c7d716d677559da
Justin Chazen <Justinchazen96@gmail.com>
2022-04-18 15:59:43 -0400

    added images to articles

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -1,12 +1,16 @@
 const StoryCard = ({element}) => {
 
     const {title, abstract, url, byline} = element
+    const image = element.multimedia[1].url
+    const caption = element.multimedia[1].caption
 
     return(
         <div>
             <h4>Title: {title}</h4>
+            <img src={image} alt={title}/>
+            <p>{caption}</p>
             <p>{abstract}</p>
-            <h4>By: {byline}</h4>
+            <h4>{byline}</h4>
             <a href={url} target={"_blank"}>Article URL</a>
         </div>
     )
