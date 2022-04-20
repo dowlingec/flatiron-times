8dc6c2c8747edcd8347299ad064c8ee1cfebea60
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-20 13:03:33 -0400

    mvp route on page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,19 @@ const Navbar = () => {
 
     return (
         <div className="nav">
-           <div className='left-nav'> <NavLink to="/" exact> Flatiron Times </NavLink></div>
-           <div className='mid-nav'> <NavLink to="/topstories" exact> Top Stories </NavLink></div>
-           <div className='right-nav'> <NavLink to="/topbooks" exact> Top Books </NavLink></div>
+           <div className='left-nav'>
+               <NavLink to="/" exact> Flatiron Times </NavLink>
+            </div>
+           <div className='mid-nav'>
+               <NavLink to="/topstories" exact>Top Stories </NavLink>
+            </div>
+           <div className='right-nav'>
+               <NavLink to="/topbooks" exact> Top Books </NavLink>
+            </div>
+            <div className="end-nav">
+                <NavLink to="/fakenews">Fake News</NavLink>
+            </div>
+
         </div>
     )
 }
