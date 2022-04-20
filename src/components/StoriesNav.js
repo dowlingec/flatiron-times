86c5e2ece6403fd4180728bea0023ae9dea4add1
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-20 09:45:39 -0400

    inputfield, sumbit button, grabbing value

--- src/components/StoriesNav.js
+++ src/components/StoriesNav.js
@@ -1,7 +1,18 @@
+import { useState } from 'react'
 const StoriesNav = () => {
+    let [filterBy, setFilterBy] = useState("")
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        return alert('Hello')
+    }
+
     return(
         <div>
-
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Find a story..." onChange={(e) => {setFilterBy(e.target.value)}}></input>
+                <button type="submit" >GO!</button>
+            </form>
+            console.log({filterBy})
         </div>
     )
 }
--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -1,8 +1,11 @@
 import StoryCard from './StoryCard'
+import StoriesNav from './StoriesNav'
 
 const TopStories = ({stories}) => {
     return(
+
         <div className='top-stories'>
+            <StoriesNav />
             {stories.map((element)=>{
                return( <StoryCard element={element} key={element.url}/>)
             })}
